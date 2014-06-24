4efc131046c9c24c9a8f7f86e73a5e8307354b66
andresfelipegarcia <felipehonor@gmail.com>
2014-06-24 10:40:43 -0500

    Proyecto danzas
    Esperemos encuentren el proyecto panas

---
+++ index.php
@@ -0,0 +1,217 @@
+
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+
+<html>
+<head>
+    <meta http-equiv="X-UA-Compatible" content="IE=9,chrome=1"/>
+    <meta charset="utf-8">
+    <title>Artista Musical Plantilla Web | WIX</title>
+    
+<!--    probando el proyecto-->
+    <meta name="description" content="Sube el volumen con esta plantilla web intensa y audaz. Es el lugar perfecto para compartir canciones, videos y noticias con tus fans. Sube fotos y personaliza el diseño y color para que vayan con el estilo de tu banda. ">
+    <meta name="author" content="Wixpress">
+    <meta http-equiv="content-language" content="es"/>
+    <meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+
+    <meta property="og:title" content="Artista Musical Plantilla Web | WIX"/>
+    <meta property="og:type" content="website"/>
+    <meta property="og:url" content="http://es.wix.com/website-template/view/html/772"/>
+    <meta property="og:image" content="http://static.wix.com/media/7bab5d2d29475be517ab6fec46fb875a.wix_mp"/>
+    <meta content="Wix" property="og:site_name">
+    <meta property="og:description" content="Sube el volumen con esta plantilla web intensa y audaz. Es el lugar perfecto para compartir canciones, videos y noticias con tus fans. Sube fotos y personaliza el diseño y color para que vayan con el estilo de tu banda. "/>
+    <meta property="fb:admins" content="731184828"/>
+    <meta name="fb:app_id" content="236335823061286"/>
+    <meta name="google-site-verification" content="QXhlrY-V2PWOmnGUb8no0L-fKzG48uJ5ozW0ukU7Rpo"/>
+
+    <link rel="canonical" href="http://es.wix.com/website-template/view/html/772"/>
+
+            <link rel="alternate" hreflang="de" href="http://de.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="it" href="http://it.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="tr" href="http://tr.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="ko" href="http://ko.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="pt" href="http://pt.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="pl" href="http://pl.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="fr" href="http://fr.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="en" href="http://www.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="ru" href="http://ru.wix.com/website-template/view/html/772"/>
+            <link rel="alternate" hreflang="ja" href="http://ja.wix.com/website-template/view/html/772"/>
+    
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="Vista/css/reset.css"/>
+    <link rel="stylesheet" type="text/css" href="Vista/css/fonts.css"/>
+    <link rel="stylesheet" type="text/css" href="Vista/css/fontFace.css"/>
+    <link rel="stylesheet" type="text/css" href="Vista/css/template_viewer.css"/>
+
+
+    <!--[if lte IE 9]>
+    <link rel="stylesheet" type="text/css" href="http://static.parastorage.com/services/wix-html-template-viewer/1.541.0/css/ie9-and-down.css"/>
+    <![endif]-->
+
+    </head>
+<body lang="es">
+<!-- Google Tag Manager -->
+    <noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-MDD5C4"
+                      height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push(
+            {'gtm.start': new Date().getTime(),event:'gtm.js'}
+    );var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-MDD5C4');</script>
+<!-- End Google Tag Manager -->
+    
+<script type="text/javascript">
+    var backUrl = "http:\/\/www.wix.com\/website\/templates";
+    var editUrl = "http://editor.wix.com/html/editor/web/renderer/new?metaSiteId=12950297-c40f-0ab4-0ffd-b52e7269933f&siteId=7780d359-bab9-4668-8548-651497073b8f";
+    var galleryDocIndex = "$pageModel.galleryDocIndex";
+    var originAppUrl = "$pageModel.unsafeJSoriginAppUrl";
+    var W = W || {};
+    W.App = W.App || {};
+    W.App.CLIENT_CONFIG = {"biServerUrl":"http://frog.wix.com/spettatore_bi?src=2&openingSiteId=7780d359-bab9-4668-8548-651497073b8f&originAppUrl=http%3A%2F%2Fwww.wix.com%2Fwebsite%2Ftemplates&siteType=2"};
+    W.App.currentViewMode = 'desktop';
+
+    W.Globals = W.Globals || {};
+    W.Globals.lang =  "es";
+    var analytics = {"staging":false,"code":"UA-2117194-29","domain":"wix.com","server":null};
+    var openAsTab = true;
+
+    //Storing dynamic text for responsive design
+    var headerTxt = {};
+    headerTxt.editButtonSmall = 'Editar';
+    headerTxt.mainHeaderSmall = 'Crea tu increíble página web ';
+</script>
+
+<!-- FACEBOOK CODE API-->
+<div id="fb-root"></div>
+<script>
+    /* Facebook Like Button*/
+    (function (d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s);
+        js.id = id;
+        js.src = "//connect.facebook.net/en_US/all.js#xfbml=1";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+
+    /*Google+ */
+    (function () {
+        var po = document.createElement('script');
+        po.type = 'text/javascript';
+        po.async = true;
+        po.src = 'https://apis.google.com/js/plusone.js';
+        var s = document.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(po, s);
+    })();
+</script>
+<!-- End of FACEBOOK CODE -->
+
+<div id="container" class="template_viewer">
+    <div id="header">
+        <div class="navigation">
+            <div id="wixLogo">
+                <a href="http://es.wix.com/">Wix Press</a>
+            </div>
+                            <div id="template-mode-toggle">
+                    <div class="view-mode-toggle-button view-mode-toggle-button-desktop" data="desktop" title="Visualización Desktop"></div>
+                    <div class="view-mode-toggle-button view-mode-toggle-button-mobile" data="mobile" title="Visualización Móvil"></div>
+                </div>
+                        <div class="templateDetails" itemscope itemtype="http://schema.org/Product/WebsiteTemplate">
+                <meta itemprop="name" content="Artista Musical"/>
+            </div>
+            <!-- End of .templateDetails-->
+            <div class="templatesButtons">
+                <span id="templatesName">Haz clic en Editar y crea tu increíble página web </span> <a id="readMore"
+                                                                                                           href="#">Leer más</a>
+                <meta itemprop="image" content="http://static.wix.com/media/7bab5d2d29475be517ab6fec46fb875a.wix_mp"/>
+                <a href="#" class="button" id="editButton">Editar este sitio web<span>&nbsp;</span></a>
+                <span id="categoriesHolder">
+
+                </span>
+                <span class="priceTypeWrapper"> Precio: <span
+                        id="priceType">Gratis</span></span>
+                <span class="html5icon"></span>
+
+                <div class="socialButtons">
+                    <div class="socialButtonsContent">
+                        <div class="facebookWrapper">
+                            <div class="fb-like" data-send="false" data-layout="button_count"
+                                 data-show-faces="true"></div>
+                        </div>
+                        <div class="googlePlusWrapper">
+                            <g:plusone size="medium"></g:plusone>
+                        </div>
+                    </div>
+                    <!-- end of .socialButtonsContent -->
+                </div>
+                <!-- end of .socialButtons -->
+            </div>
+        </div>
+        <!-- End of .navigation-->
+    </div>
+    <!-- End of #Header-->
+    <div id="headerShadows">
+        <div class="rightShadow"></div>
+        <div class="leftShadow"></div>
+    </div>
+    <!-- End of #HeaderShadows-->
+    <div class="contentWrapper">
+                    <div class="preview-tab preview-tab-desktop">
+                <iframe src="http://es.wix.com/demone2/red-music" width="100%" height="100%"></iframe>
+            </div>
+        
+        <!--</div>--><!-- End of .content-->
+    </div>
+    <!-- End of .contentWrapper-->
+
+    <div id="dialog-overlay"></div>
+    <div id="dialog-box">
+        <a id="close-pop-up-button" href="#">Close Pop Up Button</a>
+        <div class="dialog-content">
+            <h1 class="dialog-title">&#8216;Artista Musical&#8217; - Website Template</h1>
+            <div class="leftColumn">
+                <h3>Excelente para:</h3>
+
+                <div id="dialog-good-for">Bandas, músicos, artistas y creativos</div>
+                <h3>Descripción:</h3>
+
+                <div id="dialog-description" itemprop="description">Sube el volumen con esta plantilla web intensa y audaz. Es el lugar perfecto para compartir canciones, videos y noticias con tus fans. Sube fotos y personaliza el diseño y color para que vayan con el estilo de tu banda. </div>
+            </div>
+            <div class="rightColumn">
+                                                                <span>
+					<h3>Dificultad:&nbsp;</h3>
+					<div id="dialog-difficulty" itemprop="difficulty">Fácil</div>
+				</span>
+				<span itemprop="author" itemscope itemtype="http://schema.org/Organization">
+					<h3>Autor:&nbsp;</h3>
+					<div id="dialog-author" itemprop="name">Wix Studio</div>
+				</span>
+				<span itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+					<h3>Precio:&nbsp;</h3>
+					<div id="dialog-price" itemprop="price">Gratis</div>
+					<link itemprop="availability" href="http://schema.org/InStock"/>
+				</span>
+                <a href="#" id="dialog-edit-now">Editar ahora</a>
+            </div>
+            <div class="clearBoth">&nbsp;</div>
+        </div>
+        <div class="dialog-box-shadows">
+            <div class="leftShadow"></div>
+            <div class="rightShadow"></div>
+        </div>
+    </div>
+    <!-- End of #dialog-box-->
+
+</div>
+<!-- End of #container-->
+
+<!--Scripts after page loads-->
+<script src="Vista/js/jquery.js" type="text/javascript"></script>
+<script type="text/javascript" src="Vista/js/custom-scrollbar.js"></script>
+<script type="text/javascript" src="Vista/js/common.js"></script>
+<script src="Vista/js/template_viewer.js" type="text/javascript"></script>
+
+        
+
+</body>
+</html>
